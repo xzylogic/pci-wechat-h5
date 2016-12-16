0508e072ea5a04110c40c54a0889312ed97f6f2a
wangbing <120334644@qq.com>
2016-12-16 11:20:47 +0800

    lecture

--- server/controllers/lecture/apply.controller.js
+++ server/controllers/lecture/apply.controller.js
@@ -5,8 +5,18 @@ var requestTool = require('../common/request-tool');
 module.exports = {
 
   getApply: (req, res) => {
-    res.render('lecture/apply', {
-
+   requestTool.getwithhandle('lecture',"", (_data) => {
+        if (_data) {
+          res.render('lecture/apply', {
+              "json":_data,
+              name:_data[0].name,
+              errorMessage: ''
+            });
+        }
+      }, (err) => {
+        res.render('lecture/apply', {
+          errorMessage: err,
+        });
       });
 
   },
@@ -18,18 +28,17 @@ module.exports = {
     req.addListener('data', (data) => {
       postData += data;
     });
-
     req.addListener('end', () => {
       let codeData = querystring.parse(postData);
-      console.log(`POST login ${JSON.stringify(codeData)}`);
+      res.render('lecture/apply-success');
 
-      requestTool.post(res, 'lecture', codeData, (_data) => {
-        if (JSON.parse(_data).code == 0) {
-          res.render('lecture/apply-success');
-        } else {
-          res.render('lecture/apply');
-        }
-      })
+      // requestTool.post(res, 'apply', codeData, (_data) => {
+      //   if (JSON.parse(_data).code == 0) {
+          
+      //   } else {
+      //     res.render('lecture/apply');
+      //   }
+      // })
     });
   },
 
