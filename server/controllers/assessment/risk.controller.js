d540bb4dfa5d0dd3d12abf827f98aee4360f7a7a
violet_qqy <forviolet771@gmail.com>
2016-12-27 10:58:49 +0800

    risk redirect url bug gix

--- server/controllers/assessment/risk.controller.js
+++ server/controllers/assessment/risk.controller.js
@@ -8,7 +8,7 @@ var moment = require("moment");
 module.exports = {
 
   getRisk: (req, res) => {
-    let url = requestTool.setAuthUrl('assessment/risk', ''); // 重定向url
+    let url = requestTool.setAuthUrl('/assessment/risk', ''); // 重定向url
     auth.getOpenId(req, res, url, (openId) => {
       requestTool.getwithhandle('result', `openId=${openId}`, (result) => {
         if (result) {
