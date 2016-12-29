b99ae7a77463698e4d4112f620cbf99e56f26ffa
wangbing <120334644@qq.com>
2016-12-29 16:00:51 +0800

    lecture

--- server/controllers/lecture/info.controller.js
+++ server/controllers/lecture/info.controller.js
@@ -22,50 +22,55 @@ module.exports = {
 
   getInfo: (req, res) => {
   		let openId = req.signedCookies.pci_secret || ''; // 从cookie中找openId
-  		requestTool.getwithhandle('applyList',`openId=${openId}`, (_data) => {
-        if (_data) {
-	    	for (var i = 0; i < _data.content.length; i++) {
-	    		_data.content[i].time = moment(_data.content[i].time).format('YYYY-MM-DD h:mm');
-	    		_data.content[i].day = new Date(_data.content[i].time).getDay();
-	    		if(_data.content[i].day == 0){
-	    			_data.content[i].day = "日";
-	    		}else if(_data.content[i].day == 1){
-	    			_data.content[i].day = "一";
-	    		}else if(_data.content[i].day == 2){
-	    			_data.content[i].day = "二";
-	    		}else if(_data.content[i].day == 3){
-	    			_data.content[i].day = "三";
-	    		}else if(_data.content[i].day == 4){
-	    			_data.content[i].day = "四";
-	    		}else if(_data.content[i].day == 5){
-	    			_data.content[i].day = "五";
-	    		}else if(_data.content[i].day == 6){
-	    			_data.content[i].day = "六";
-	    		};
-	    		if(_data.content[i].status == 0){
-	    			_data.content[i].statusName = "报名成功";
-	    		}else if(_data.content[i].status == 1){
-	    			_data.content[i].statusName = "报名取消";
-	    		}else if(_data.content[i].status == 2){
-	    			_data.content[i].statusName = "报名签到";
-	    		}else if(_data.content[i].status == 3){
-	    			_data.content[i].statusName = "报名过期";
-	    		}
-	    		if(_data.content[i].charge == 0){
-	    			_data.content[i].charge = "免费";
-	    		}
-	    	}
+      if(openId){
+        requestTool.getwithhandle('applyList',`openId=${openId}`, (_data) => {
+          if (_data) {
+            for (var i = 0; i < _data.content.length; i++) {
+              _data.content[i].time = moment(_data.content[i].time).format('YYYY-MM-DD h:mm');
+              _data.content[i].day = new Date(_data.content[i].time).getDay();
+              if(_data.content[i].day == 0){
+                _data.content[i].day = "日";
+              }else if(_data.content[i].day == 1){
+                _data.content[i].day = "一";
+              }else if(_data.content[i].day == 2){
+                _data.content[i].day = "二";
+              }else if(_data.content[i].day == 3){
+                _data.content[i].day = "三";
+              }else if(_data.content[i].day == 4){
+                _data.content[i].day = "四";
+              }else if(_data.content[i].day == 5){
+                _data.content[i].day = "五";
+              }else if(_data.content[i].day == 6){
+                _data.content[i].day = "六";
+              };
+              if(_data.content[i].status == 0){
+                _data.content[i].statusName = "报名成功";
+              }else if(_data.content[i].status == 1){
+                _data.content[i].statusName = "报名取消";
+              }else if(_data.content[i].status == 2){
+                _data.content[i].statusName = "报名签到";
+              }else if(_data.content[i].status == 3){
+                _data.content[i].statusName = "报名过期";
+              }
+              if(_data.content[i].charge == 0){
+                _data.content[i].charge = "免费";
+              }
+            }
+                res.render('lecture/info', {
+                  data:_data.content,
+                  errorMessage:""
+                });
+            }
+          }, (err) => {
             res.render('lecture/info', {
-              data:_data.content,
-              errorMessage:""
+              data:[],
+               errorMessage: err,
             });
-        }
-      }, (err) => {
-        res.render('lecture/info', {
-        	data:[],
-           errorMessage: err,
         });
-      });
+      }else{
+        res.redirect(`${global.config.root}/lecture/info`);
+      }
+  		
      
   },
 
