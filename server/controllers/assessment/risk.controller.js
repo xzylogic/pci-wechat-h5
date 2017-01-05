4356050c654eceab12b19cfe304e70006f18ced4
violet_qqy <forviolet771@gmail.com>
2017-01-05 17:57:15 +0800

    123

--- server/controllers/assessment/risk.controller.js
+++ server/controllers/assessment/risk.controller.js
@@ -37,27 +37,32 @@ module.exports = {
   },
 
   getRiskEnter: (req, res) => {
-    let openId = req.signedCookies.pci_secret || ''; // 从cookie中找openId
-    if (openId) {
-      res.render('assessment/risk');
-    } else {
-      res.redirect(`${global.config.root}/assessment/risk`);
-    }
+    // auth.setCookies(res, 'pci_secret', 'ox0ThwmPe29gK2bl8v7cbr6Z-emg');
+    let url = requestTool.setAuthUrl('/assessment', ''); // 重定向url
+    auth.getOpenId(req, res, url, (openId) => {
+      res.render('assessment/risk', {
+        url: `${global.config.root}/assessment/verify?openId=${openId}`
+      });
+    }, (err) => {
+      res.render('error', {
+        message: err
+      });
+    });
   },
 
   // 提交风险评估接口
   riskVerify: (req, res) => {
-    let openId = req.signedCookies.pci_secret || ''; // 从cookie中找openId
+    let openId = req.query.openId;
     if (openId) {
       let postData = req.body;
       postData.openId = openId;
       console.log(`[${new Date()}] Risk Post Data: ${JSON.stringify(postData)}`);
       requestTool.postApi(res, 'risk', postData, (_data) => {
-        console.log(_data);
-        res.send(_data);
+        console.log(JSON.parse(_data));
+        res.send(JSON.parse(_data));
       });
     } else {
-      res.redirect(`${global.config.root}/assessment/risk`);
+      res.send({ code: -1, msg: "请求错误" });
     }
   }
 }
