14175fa76217ecf693a8e554a32546bd20fb7080
wangbing <120334644@qq.com>
2017-12-14 10:16:07 +0800

    修改BUG

--- server/controllers/healthRecords/followupPlan.controller.js
+++ server/controllers/healthRecords/followupPlan.controller.js
@@ -64,9 +64,14 @@ module.exports = {
             userId: data.userId
           }
           requestTool.postHeader('flupDetail', data.access_token, postData, (_data) =>{
-            if (_data && _data.code === 0) {
+            if (_data && _data.code === 0 && _data.data) {
+              let date = _data.data.planDate.split('-');
+              console.log(date)
               res.render('healthRecords/followupPlanDetail',{
-                flupDetail: _data.data
+                flupDetail: _data.data,
+                Y: Number(date[0]),
+                M: Number(date[1]),
+                D: Number(date[2])
               })
             } else if (_data && _data.code === 403) {
               res.clearCookie('accessToken');
@@ -100,42 +105,84 @@ module.exports = {
 
   //随访计划反馈
   getFollowFeedback: (req, res) => {
-    let fbId = req.query.fbId || 1;
+    let fbId = req.query.fbId || '';
     let otherRemind = req.query.otherRemind || '';
     let feedbackTimes = req.query.feedbackTimes || '';
-    let dn = req.query.feedbackTimes || '';
+    let dn = req.query.dn || '';
     let url = requestTool.setAuthUrl('/followfeedback', '');
+    console.log(querystring.parse(dn))
     auth.getOpenId(req, res, url, (openId) => {
       auth.isLogin(req, (data) => {
-        requestTool.getHeaderUrl(`api/doctorPatient/flup/feedback/find/${fbId}`, data.access_token, '', (_data) =>{
-          console.log(_data)
-          if (_data.code === 0 && _data.data.plan && _data.data.plan.length !== 0 && _data.data.firstPushStatus === true) {
-            res.render('healthRecords/followfeedback',{
-              plan: _data.data.plan,
-              item: _data.data.item,
-              otherRemind: otherRemind,
-              feedbackTimes: feedbackTimes,
-              flupFeedbackId: fbId,
-              dn: dn,
-              url: global.config.userServer,
-              access_token: data.access_token
-            })
-          } else if (_data.code === 0 && _data.data.firstPushStatus === null || _data.data.firstPushStatus === false) {
-              res.redirect(`${global.config.root}/followfailure`);
-          } else if (_data.code === 403) {
-            res.clearCookie('accessToken');
-            res.clearCookie('userId');
-            res.redirect(`${global.config.root}/login?status=6`);
-          } else {
+        if (feedbackTimes == 0) {
+          requestTool.getHeaderUrl(`api/doctorPatient/flup/feedback/find/${fbId}`, data.access_token, '', (_data) =>{
+            if (_data.code === 0 && _data.data && _data.data.plan && _data.data.plan.length !== 0 && _data.data.item && !_data.data.firstPushStatus) {
+              res.render('healthRecords/followfeedback',{
+                plan: _data.data.plan,
+                item: _data.data.item,
+                otherRemind: otherRemind,
+                feedbackTimes: feedbackTimes,
+                flupFeedbackId: fbId,
+                dn: querystring.parse(dn),
+                url: global.config.userServer,
+                access_token: data.access_token
+              })
+            } else if (_data.code === 0 && _data.data && _data.data.firstPushStatus) {
+                res.redirect(`${global.config.root}/followfailure`);
+            } else if (_data.code === 403) {
+              res.clearCookie('accessToken');
+              res.clearCookie('userId');
+              res.redirect(`${global.config.root}/login?status=6`);
+            } else {
+              res.render('error', {
+                message: _data.msg || '未知错误'
+              });
+            }
+          }, (error) => {
             res.render('error', {
-              message: _data.msg || '未知错误'
+              message: error
             });
-          }
-        }, (error) => {
+          })
+        } else if (feedbackTimes == 1) {
+          requestTool.getHeaderUrl(`api/doctorPatient/flup/feedback/find/${fbId}`, data.access_token, '', (_data) =>{
+            if (_data.code === 0 && _data.data && _data.data.plan && _data.data.plan.length !== 0 && _data.data.item && !_data.data.secondPushStatus) {
+              let a = new Set(_data.data.plan);
+              let b = new Set(_data.data.item);
+              let differenceABSet = new Set([...a].filter(x => !b.has(x)));
+              let plan = [];
+              differenceABSet.forEach(function(obj){
+                plan.push(obj)
+              })
+              res.render('healthRecords/followfeedback',{
+                plan: plan,
+                item: _data.data.item,
+                otherRemind: otherRemind,
+                feedbackTimes: feedbackTimes,
+                flupFeedbackId: fbId,
+                dn: dn,
+                url: global.config.userServer,
+                access_token: data.access_token
+              })
+            } else if (_data.code === 0 && _data.data && _data.data.secondPushStatus) {
+                res.redirect(`${global.config.root}/followfailure`);
+            } else if (_data.code === 403) {
+              res.clearCookie('accessToken');
+              res.clearCookie('userId');
+              res.redirect(`${global.config.root}/login?status=6`);
+            } else {
+              res.render('error', {
+                message: _data.msg || '请求错误'
+              });
+            }
+          }, (error) => {
+            res.render('error', {
+              message: error
+            });
+          })
+        } else {
           res.render('error', {
-            message: error
+            message: '请求错误'
           });
-        })
+        }
       }, () => {
         // 未登录跳转登录页面
         res.redirect(`${global.config.root}/login?status=6`);
