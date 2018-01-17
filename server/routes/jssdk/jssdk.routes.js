1a3641e07b2c4911188cbebfc7290fe5d6f5094e
wangbing <120334644@qq.com>
2018-01-17 10:12:41 +0800

    修改上传图片方法

--- server/routes/jssdk/jssdk.routes.js
+++ server/routes/jssdk/jssdk.routes.js
@@ -7,6 +7,5 @@ var JssdkController = require('../../controllers/jssdk/jssdk.controller');
 router.route('/signature').get(JssdkController.getSignature); // 获取config配置参数
 router.route('/reset').get(JssdkController.resetLogin); // 清除cookie
 router.route('/share').get(JssdkController.getShare); // 分享页面
-router.route('/uploadimg').post(JssdkController.uploadImg); // 上传图片
 
 module.exports = router;
